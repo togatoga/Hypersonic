e4b8c2f75486a6e4da1fdf567c81517ed86b4f59
togatoga <togasakitogatoga@gmail.com>
2016-09-25 07:24:22 +0900

    Implement Timer

--- Solver.cpp
+++ Solver.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+#include <sys/time.h>
 using namespace std;
+/*
+--------------------------Template--------------------------
+*/
+class Timer{
+public:
+  Timer(){}
+  void start(){
+    start_time = get_mill_sec();
+  }
+  double get_mill_duration(){
+    return get_mill_sec() - start_time;
+  }
+  
+private:
+  double start_time;
+  double get_mill_sec(){
+    struct timeval res;
+    gettimeofday(&res, NULL);
+    return res.tv_sec * 1000 + res.tv_usec / 1000;
+  }
+};
+
+/*
+---------------------------Game Code---------------------------
+*/
 
 const int8_t BOARD_WIDTH = 13;
 const int8_t BOARD_HEIGHT = 11;
