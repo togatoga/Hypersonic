702b63e68db68821e7eabdfe04e11d2364fa894e
togatoga <togasakitogatoga@gmail.com>
2016-09-29 02:26:08 +0900

    Modify calc_score

--- Solver.cpp
+++ Solver.cpp
@@ -623,8 +623,8 @@ private:
     score *= 100;
 
     score += 20 * (search_state.state.players[id].sum_box_point);
-    score += 5 * (search_state.state.players[id].explosion_range - 3);
-    score += 5 * (search_state.state.players[id].max_bomb_cnt - 1);
+    score += 3 * (min(13, search_state.state.players[id].explosion_range) - 3);
+    score += 3 * (min(7, search_state.state.players[id].max_bomb_cnt) - 1);
     score += (search_state.state.players[id].remain_bomb_cnt);
     
     score *= 100;
